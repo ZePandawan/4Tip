f84c0621e268f2b2a0ab9727cd42cd09fd55581f
Vincent_Reix_54 <vincentreix54@gmail.com>
2023-12-11 11:04:11 +0100

    Fix warns and ban automatically user if he has 3 warns

--- Code/commands/warn.ts
+++ Code/commands/warn.ts
@@ -11,9 +11,14 @@ exports.run = async (client, interaction) => {
     // isUserAdmin = false --> pas admin
 
     if (isUserAdmin) {
-        addWarn(interaction.guildId, member_to_warn.id, reason, Date.now(), nb_warn, interaction.member.id);
-        console.log(member_to_warn);
-        await interaction.reply(`L'utilisateur ${member_to_warn.user.username} a été averti`);
+        if (nb_warn >= 3) {
+            await member_to_warn.ban({ deleteMessageSeconds: 60 * 60 * 24 * 7, reason: '3 warns => Ban' });
+            await interaction.reply(`L'utilisateur ${member_to_warn.user.username} a été banni car 3 warns`);
+        } else {
+            addWarn(interaction.guildId, member_to_warn.id, reason, Date.now(), nb_warn, interaction.member.id);
+            //console.log(member_to_warn);
+            await interaction.reply(`L'utilisateur ${member_to_warn.user.username} a été averti`);
+        }
     } else {
         await interaction.reply("Vous n'avez pas accès à cette commande");
     }
