48799ec3cb16082a66d30fb30acddda39fe92fc4
ZePandawan <vincentreix54@gmail.com>
2024-11-18 13:49:45 +0100

    Verify if channel type is text or not

--- Code/commands/define-channel.js
+++ Code/commands/define-channel.js
@@ -1,19 +1,25 @@
 const fs = require("fs");
 
 exports.run = async (client, interaction) => {
+    // Récupération des options
     const channel = interaction.options.getChannel("channel");
     const type = interaction.options.getString("type");
     const guildId = interaction.guildId;
 
+    // Ouverture de la BDD
     const dbFile = `./Code/database/${guildId}.json`;
     const data = JSON.parse(fs.readFileSync(dbFile, "utf-8"));
 
-    /*if(!channel.isText()){
+    // Vérification du type de salon
+    if(channel.type !== 0){
         interaction.reply("Le salon doit être un salon textuel !");
         return;
-    }*/
+    }
 
+    // Vérification de l'existence du salon dans la BDD
     const channelExistIndex = data.channels.findIndex(channel => channel[type] );
+
+    // Si le salon existe, on le met à jour, sinon on le crée
     if(channelExistIndex !== -1){
         data.channels[channelExistIndex][type] = channel.id;
     }else{
@@ -23,11 +29,9 @@ exports.run = async (client, interaction) => {
         data.channels.push(channelData);
     }
 
+    // Sauvegarde des données
     const updatedData = JSON.stringify(data, null, 2);
     fs.writeFileSync(dbFile, updatedData);
 
-
-    //console.log(channelData);
     interaction.reply(`Le salon ${channel} a bien été défini comme salon ${type} !`);
-
 }
