cc95f5463436d5975d13c693c477a655d4849dd0
ZePandawan <vincentreix54@gmail.com>
2025-03-28 18:02:38 +0100

    Now poke-inventory has button to change pages

--- Code/commands/poke-inventory.js
+++ Code/commands/poke-inventory.js
@@ -1,127 +1,107 @@
-const {EmbedBuilder} = require('discord.js');
-
-const {getUserPokemon, APIRequest} = require("../functions");
+const { EmbedBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const paginationEmbed = require('discordjs-v14-pagination');
+const { getUserPokemon } = require("../functions");
 
 exports.run = async (client, interaction) => {
+    
+
     const type = interaction.options.getString("type");
     const pokemonAsk = interaction.options.getString("pokemon");
     const doublons = interaction.options.getBoolean("doublons");
     const isShiny = type === "shiny";
     const pokeList = getUserPokemon(interaction.user.id, isShiny);
-    const page = interaction.options.getNumber("page");
-    let inventory = "";
-    const page_size = 10;
-    const start_index = (page - 1) * page_size;
-    let end_index = 0;
-    pokeList.length < start_index + page_size ? end_index = pokeList.length: end_index = start_index + page_size;
-    const total_pages = Math.ceil(pokeList.length / page_size);
+    const pageSize = 10;
 
     if (doublons) {
-        // Filter the list to only include Pokémon with quantity greater than 1
         const doublesList = pokeList.filter(pokemon => pokemon.quantity > 1);
 
         if (doublesList.length === 0) {
-            const pokeEmbed = new EmbedBuilder()
+            return interaction.editReply({
+                embeds: [
+                    new EmbedBuilder()
+                        .setColor(0x0099FF)
+                        .setTitle('POKEMON 4TIP')
+                        .addFields({ name: `Inventaire de ${interaction.user.displayName}`, value: `Vous n'avez pas de Pokémon en double.` })
+                        .addFields({ name: "Pokedex :", value: `${pokeList.length}/1018` })
+                        .setTimestamp()
+                        .setFooter({ text: '4Tip' })
+                ]
+            });
+        }
+
+        const pages = doublesList.reduce((acc, pokemon, index) => {
+            const pageIndex = Math.floor(index / pageSize);
+            acc[pageIndex] = acc[pageIndex] || "";
+            acc[pageIndex] += `${pokemon.name} x${pokemon.quantity}\n`;
+            return acc;
+        }, []);
+
+        const embeds = pages.map((inventory, i) =>
+            new EmbedBuilder()
                 .setColor(0x0099FF)
                 .setTitle('POKEMON 4TIP')
-                .addFields({ name: `Inventaire de ${interaction.user.displayName}`, value: `Vous n'avez pas de Pokémon en double.` })
+                .setDescription(`Page ${i + 1}/${pages.length}`)
+                .addFields({ name: `Inventaire de ${interaction.user.displayName}`, value: inventory })
                 .addFields({ name: "Pokedex :", value: `${pokeList.length}/1018` })
                 .setTimestamp()
-                .setFooter({ text: '4Tip' });
-            await interaction.reply({ embeds: [pokeEmbed] });
-            return;
-        }
+                .setFooter({ text: '4Tip' })
+        );
 
-        // Paginate the doubles list
-        const doublesTotalPages = Math.ceil(doublesList.length / page_size);
-        const doublesStartIndex = (page - 1) * page_size;
-        let doublesEndIndex = 0;
-        doublesList.length < doublesStartIndex + page_size ? doublesEndIndex = doublesList.length : doublesEndIndex = doublesStartIndex + page_size;
+        const buttons = [
+            new ButtonBuilder().setCustomId('first').setEmoji('⏮').setStyle(ButtonStyle.Primary),
+            new ButtonBuilder().setCustomId('previous').setEmoji('◀').setStyle(ButtonStyle.Primary),
+            new ButtonBuilder().setCustomId('next').setEmoji('▶').setStyle(ButtonStyle.Primary),
+            new ButtonBuilder().setCustomId('last').setEmoji('⏭').setStyle(ButtonStyle.Primary)
+        ];
 
-        if (page < 1 || page > doublesTotalPages) {
-            await interaction.reply(`Numéro de page incorrect, il doit être compris entre 1 et ${doublesTotalPages}`);
-            return;
-        }
+        return paginationEmbed(interaction, embeds, buttons, 60000, 'Page {current}/{total}');
+    }
 
-        for (let i = doublesStartIndex; i < doublesEndIndex; i++) {
-            inventory += `${doublesList[i].name} x${doublesList[i].quantity}\n`;
-        }
+    if (pokemonAsk) {
+        const pokemonResult = pokeList.find(pokemon => pokemon.name.toLowerCase() === pokemonAsk.toLowerCase());
 
-        const pokeEmbed = new EmbedBuilder()
+        return interaction.editReply({
+            embeds: [
+                new EmbedBuilder()
+                    .setColor(0x0099FF)
+                    .setTitle('POKEMON 4TIP')
+                    .addFields({
+                        name: `Inventaire de ${interaction.user.displayName}`,
+                        value: pokemonResult
+                            ? `${pokemonResult.name} x${pokemonResult.quantity}`
+                            : `*Vous ne possédez pas le pokémon ${pokemonAsk}. Essayez de le capturer avec /pokemon*`
+                    })
+                    .addFields({ name: "Pokedex :", value: `${pokeList.length}/1018` })
+                    .setTimestamp()
+                    .setFooter({ text: '4Tip' })
+            ]
+        });
+    }
+
+    const pages = pokeList.reduce((acc, pokemon, index) => {
+        const pageIndex = Math.floor(index / pageSize);
+        acc[pageIndex] = acc[pageIndex] || "";
+        acc[pageIndex] += `${pokemon.name} x${pokemon.quantity}\n`;
+        return acc;
+    }, []);
+
+    const embeds = pages.map((inventory, i) =>
+        new EmbedBuilder()
             .setColor(0x0099FF)
             .setTitle('POKEMON 4TIP')
-            .setDescription(`Page ${page}/${doublesTotalPages}`)
-            .addFields({ name: `Inventaire de ${interaction.user.displayName}`, value: `${inventory}` })
+            .setDescription(`Page ${i + 1}/${pages.length}`)
+            .addFields({ name: `Inventaire de ${interaction.user.displayName}`, value: inventory })
             .addFields({ name: "Pokedex :", value: `${pokeList.length}/1018` })
             .setTimestamp()
-            .setFooter({ text: '4Tip' });
-        await interaction.reply({ embeds: [pokeEmbed] });
+            .setFooter({ text: '4Tip' })
+    );
 
-    } else if(pokemonAsk){
-        const pokemonResult = pokeList.find(pokemon => pokemon.name == pokemonAsk);
-        if(pokemonResult){
-            const pokeEmbed = new EmbedBuilder()
-                .setColor(0x0099FF)
-                .setTitle('POKEMON 4TIP')
-                .addFields({name: `Inventaire de ${interaction.user.displayName}`, value: `${pokemonResult.name} x${pokemonResult.quantity}`})
-                .addFields({name: "Pokedex :", value:`${pokeList.length}/1018`})
-                .setTimestamp()
-                .setFooter({text: '4Tip'});
-            await interaction.reply({embeds: [pokeEmbed]});
-        }else{
-        
-            const pokeEmbed = new EmbedBuilder()
-                .setColor(0x0099FF)
-                .setTitle('POKEMON 4TIP')
-                .addFields({name: `Inventaire de ${interaction.user.displayName}`, value: `*Vous ne possédez pas le pokémon ${pokemonAsk} essayez de le capturer avec /pokemon*`})
-                .addFields({name: "Pokedex :", value:`${pokeList.length}/1018`})
-                .setTimestamp()
-                .setFooter({text: '4Tip'});
-            await interaction.reply({embeds: [pokeEmbed]});
-        }
-    
-    }else{
+    const buttons = [
+        new ButtonBuilder().setCustomId('first').setEmoji('⏮').setStyle(ButtonStyle.Primary),
+        new ButtonBuilder().setCustomId('previous').setEmoji('◀').setStyle(ButtonStyle.Primary),
+        new ButtonBuilder().setCustomId('next').setEmoji('▶').setStyle(ButtonStyle.Primary),
+        new ButtonBuilder().setCustomId('last').setEmoji('⏭').setStyle(ButtonStyle.Primary)
+    ];
 
-        if (page) {
-            if (page < 1 || page > total_pages) {
-                await interaction.reply(`Numéro de page incorrect, il doit être compris entre 1 et ${total_pages}`);
-            } else {
-                for (let i = start_index; i < end_index; i++) {
-                    /*const api_result = await APIRequest(`https://tyradex.vercel.app/api/v1/pokemon/${pokeList[i].id}`).then(data => {
-                    return `${data.name.fr} x${pokeList[i].quantity}`;
-                });*/
-                inventory += `${pokeList[i].name} x${pokeList[i].quantity}\n`;
-            }
-            const pokeEmbed = new EmbedBuilder()
-            .setColor(0x0099FF)
-            .setTitle('POKEMON 4TIP')
-                .setDescription(`Page ${page}/${total_pages}`)
-                .addFields({name: `Inventaire de ${interaction.user.displayName}`, value: `${inventory}`})
-                .addFields({name: "Pokedex :", value:`${pokeList.length}/1018`})
-                .setTimestamp()
-                .setFooter({text: '4Tip'});
-                await interaction.reply({embeds: [pokeEmbed]});
-            }
-        } else {
-            let arraySize = 0;
-            pokeList.length < 10 ? arraySize = pokeList.length : arraySize = 10;
-            for (let i = 0; i < arraySize; i++) {
-            /*const api_result = await APIRequest(`https://tyradex.vercel.app/api/v1/pokemon/${pokeList[i].id}`).then(data => {
-                return `${data.name.fr} x${pokeList[i].quantity}`;
-                });*/
-                inventory += `${pokeList[i].name} x${pokeList[i].quantity}\n`;
-            }
-            const pokeEmbed = new EmbedBuilder()
-            .setColor(0x0099FF)
-            .setTitle('POKEMON 4TIP')
-            .setDescription(`Page 1/${total_pages}`)
-            .addFields({name: `Inventaire de ${interaction.user.displayName}`, value: `${inventory}`})
-            .addFields({name: "Pokedex :", value:`${pokeList.length}/1018`})
-            .setTimestamp()
-            .setFooter({text: '4Tip'});
-            await interaction.reply({embeds: [pokeEmbed]});
-        }
-    }
-    
+    return paginationEmbed(interaction, embeds, buttons, 300000, 'Page {current}/{total}');
 };
-
